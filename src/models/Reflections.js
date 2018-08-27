6a66f143f309d899a107dc0be1312476f5175a0a
Collins Njau <collinsnjau39@gmail.com>
2018-08-27 15:01:16 +0300

    Create method to create a reflection

---
+++ src/models/Reflections.js
@@ -0,0 +1,22 @@
+import moment from 'moment';
+import uuid from 'uuid';
+
+class Reflection {
+    constructor(){
+        this.reflections = []
+    }
+
+    // creating a reflection
+    create(data){
+        const newReflection = {
+            id: uuid.v4(),
+            success: data.success || '',
+            lowPoint: data.lowPoint || '',
+            takeAway: data.takeAway || '',
+            createdDate: moment.now(),
+            modifiedDate: moment.now()
+        };
+        this.reflections.push(newReflection);
+        return newReflection
+    }
+}
